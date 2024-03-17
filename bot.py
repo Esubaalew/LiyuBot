900607c9b6f37d72a59c0de155ffec760d697af3
Esubaalew <esubalewchekol6@gmail.com>
2024-03-17 13:37:33 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+import os
+
+from dotenv import load_dotenv
+
+
+from telegram.ext import CommandHandler, Updater
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"ሰላም, {update.effective_chat.first_name}  እንዴት ነዎ 🤗! እኔ ልዩ እባላለሁ ።")
+
+
+
+def main():
+    load_dotenv()
+    TOKEN = os.getenv('TOKEN')
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
